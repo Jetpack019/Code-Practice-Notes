6bd41664259362b1f8b403c1faf3d19e96cc7904
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2025-11-03 21:42:08 +0800

    Try fast coding todo list

---
+++ sample_list/src/component/SecondPracticeFast/Idea.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function Idea() {
+  const [data, showData] = useState([]);
+  const [title, setTitleChange] = useState("");
+  const [description, setDescriptionChange] = useState("");
+
+  function handleTitleChange(e) {
+    setTitleChange(e.target.value);
+  }
+  function handleDescriptionChange(e) {
+    setDescriptionChange(e.target.value);
+  }
+
+  function btnShowData(firstVal, secondVal) {
+    showData((prevData) => {
+      return [...prevData, { title: firstVal, description: secondVal }];
+    });
+    setTitleChange("");
+    setDescriptionChange("");
+  }
+
+  const isDisabled = title.trim() === "" || description.trim() === "";
+
+  return (
+    <div>
+      <input type="text" name="title" onChange={handleTitleChange} />
+      <input
+        type="text"
+        name="description"
+        id=""
+        onChange={handleDescriptionChange}
+      />
+
+      <button
+        onClick={() => btnShowData(title, description)}
+        disabled={isDisabled}
+      >
+        Show data
+      </button>
+      {data.map((dataval, index) => (
+        <li key={index}>
+          {dataval.title} - {dataval.description}
+        </li>
+      ))}
+    </div>
+  );
+}
+
+export default Idea;
