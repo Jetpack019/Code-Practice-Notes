4412e822677c81d1297f549b15a5827f6e36e24e
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2025-11-03 23:58:35 +0800

    button output of the operation completed: error in numbers combining simultaneously

--- sample_list/src/App.jsx
+++ sample_list/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Idea from "./component/SecondPracticeFast/Idea";
+import Calculator from "./component/Calculator/Calculator";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Idea />
+      <Calculator />
     </>
   );
 }
