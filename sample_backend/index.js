8e915ef026b183cd7ac3fd5b7e5406cf66d91624
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2025-11-04 23:03:07 +0800

    try again basic routing express

--- sample_backend/index.js
+++ sample_backend/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const router = app.router;
+const my_data = [
+  {
+    name: "Jade",
+    job: "FrontEnd Dev",
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("hello testing");
+});
+
+app.get("/post", (req, res) => {
+  res.send(my_data);
 });
 
 app.listen(3000, () => {
